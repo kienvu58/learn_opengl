cd4740a616faea275b8e0eb5bd76d8fa4ef93776
Kien Vu <vtk5995@gmail.com>
2017-02-10 00:55:01 +0700

    + hello rectangle (glDrawElements)

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -97,20 +97,38 @@ int main()
     // Set up vertex data (and buffer(s)) and attribute pointers
     GLfloat vertices[] =
     {
-        -0.5f, -0.5f, 0.0f,     // left
-         0.5f, -0.5f, 0.0f,     // right
-         0.0f,  0.5f, 0.0f      // top
+         0.5f,  0.5f, 0.0f,     // top right
+         0.5f, -0.5f, 0.0f,     // bottom right
+        -0.5f, -0.5f, 0.0f,     // bottom left
+        -0.5f,  0.5f, 0.0f      // top left
     };
-    GLuint vbo, vao;
+    GLuint indices[] =
+    {
+        0, 1, 3,
+        1, 2, 3
+    };
+    GLuint vbo, vao, ebo;
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
     glBindVertexArray(vao);
+
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);   
+
     glBindVertexArray(0);
+
+    // Uncommenting this call will result in wireframe polygons
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
     // Main loop
@@ -123,7 +141,7 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
@@ -131,6 +149,7 @@ int main()
 
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
 
     glfwTerminate();
     return 0;

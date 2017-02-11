482a37e8360a0f25959853deb64bb92ed96bc0d0
Kien Vu <vtk5995@gmail.com>
2017-02-11 23:33:37 +0700

    + load texture: container.jpg

--- LearnOpenGL/default.fs
+++ LearnOpenGL/default.fs
@@ -1,9 +1,12 @@
 #version 330
 in vec3 ourColor;
+in vec2 ourTexCoord;
 
 out vec4 color;
 
+uniform sampler2D ourTexture;
+
 void main()
 {
-    color = vec4(ourColor, 1.0f);
+    color = texture(ourTexture, ourTexCoord);
 }
--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,6 +1,7 @@
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 #include <iostream>
+#include <SOIL.h>
 
 #include "Shader.h"
 
@@ -11,6 +12,7 @@ void KeyCallBack(GLFWwindow* window, int key, int scancode, int action, int mode
 
 const char* vertexShaderPath = "default.vs";
 const char* fragmentShaderPath = "default.fs";
+const char* imagePath = "container.jpg";
 
 int main()
 {
@@ -49,17 +51,30 @@ int main()
     Shader shader(vertexShaderPath, fragmentShaderPath);
 
     // Set up vertex data (and buffer(s)) and attribute pointers
+    //GLfloat vertices[] =
+    //{
+    //    // Positions        // Colors
+    //     0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // Bottom Right
+    //    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // Bottom Left
+    //     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // Top
+    //};
+    //GLuint indices[] =
+    //{
+    //    0, 1, 2
+    //};
     GLfloat vertices[] =
     {
-        // Positions        // Colors
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // Bottom Right
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // Bottom Left
-         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // Top
+        // Positions          // Colors           // Texture Coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left 
     };
-    GLuint indices[] =
-    {
-        0, 1, 2
+    GLuint indices[] = {  // Note that we start from 0!
+        0, 1, 3, // First Triangle
+        1, 2, 3  // Second Triangle
     };
+    
 
     GLuint vbo, vao, ebo;
     glGenVertexArrays(1, &vao);
@@ -75,16 +90,37 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
     // Color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
+
+    // TexCoord attribute
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);   
 
     glBindVertexArray(0);
+
+    // Load and create a texture
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    int imgWidth;
+    int imgHeight;
+    unsigned char* image = SOIL_load_image(imagePath, &imgWidth, &imgHeight, nullptr, SOIL_LOAD_RGB);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imgWidth, imgHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
     // Uncommenting this call will result in wireframe polygons
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -98,9 +134,10 @@ int main()
         glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindTexture(GL_TEXTURE_2D, texture);
         shader.UseProgram();
         glBindVertexArray(vao);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
